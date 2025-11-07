b4fe4a43c7b556923f50b9af4e8f974c8cfa1ede
Nickss09 <nicoliemuller0iap@gmail.com>
2025-11-07 12:14:45 -0300

    continuação do app

--- src/App.jsx
+++ src/App.jsx
@@ -1,146 +1,232 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  // const [name, setName] = useState('')
+  // const [age, setAge] = useState('')
+  // const [telephone, setTelephone] = useState('')
+  // const [instagram, setInstagram] = useState('')
+  // const [github, setGithub] = useState('')
+  // const [thought, setThought] = useState('')
+  // const [problem, setProblem] = useState('')
+  // const [serie, setSerie] = useState('')
+  // const [game, setGame] = useState('')
+  // const [music, setMusic] = useState('')
+  // const [gender, setGender] = useState('')
+  // const [ability, setAbility] = useState('')
+  // const [power, setPower] = useState('')]
+  // const [team, setTeam] = useState('')]
+
   const mudancaDeEmail = (e) => {
-    setEmail(e.target.value)
+    setEmail(e.target.value);
   }
 
   const mudancaDeSenha = (e) => {
     setPassword(e.target.value)
   }
 
+  // const mudancaDeNome = (e) => {
+  //   setName(e.target.value)
+  // }
+  // const mudancaDeIdade = (e) => {
+  //   setAge(e.target.value)
+  // }
+  // const mudancaDeTelefone = (e) => {
+  //   setTelephone(e.target.value)
+  // }
+  // const mudancaDePerfil = (e) => {
+  //   setInstagram(e.target.value)
+  // }
+  // const mudancaDeUsuario = (e) => {
+  //   setGithub(e.target.value)
+  // }
+  // const mudancaDePensamento = (e) => {
+  //   setThought(e.target.value)
+  // }
+  // const mudancaDeProblema = (e) => {
+  //   setProblem(e.target.value)
+  // }
+  // const mudancaDeSerie = (e) => {
+  //   setSerie(e.target.value)
+  // }
+  // const mudancaDeJogo = (e) => {
+  //   setGame(e.target.value)
+  // }
+  // const mudancaDeMusica = (e) => {
+  //   setMusica(e.target.value)
+  // }
+  // const mudancaDeGenero = (e) => {
+  //   setGender(e.target.value)
+  // }
+  // const mudancaDeHabilidade = (e) => {
+  //   setbility(e.target.value)
+  // }
+  // const mudancaDePoder = (e) => {
+  //   set(e.target.value)
+  // }
+  // const mudancaDeTime = (e) => {
+  //   set(e.target.value)
+  // }
+
   const enviarForms = (e) => {
     e.preventDefault()
-    console.log(`Email: ${email}, Senha: ${password}`)
+    fetch('https://www.api.alanleiser.com/', {
+      method: 'POST',
+      body: JSON.stringify()
+    })
   }
-  return (
-    <>
-      <section>
-        <h2>Cadasatro de Usuário</h2>
-        <div>
-          <form onSubmit={enviarForms}> 
-            <label htmlFor="email">email</label>
+  console.log(`Email: ${email}, Senha: ${password}`)
+
+
+// useEffect(() => {
+//   fetch('https://www.api.alanleiser.com/')
+//     .then(response => response.json())
+//     .then(get => console.log(get))
+// })
+
+return (
+  <>
+    <section>
+      <h2>Cadasatro de Usuário</h2>
+      <div>
+        <form onSubmit={enviarForms}>
+          <label htmlFor="email">email</label>
+          <input
+            id='email'
+            type='email'
+            name='email'
+            value={email}
+            onChange={(e) => mudancaDeEmail(e)}
+          />
+
+          <label htmlFor="password">senha</label>
+          <input
+            id='password'
+            type='password'
+            name='senha'
+            value={password}
+            onChange={(e) => mudancaDeSenha(e)}
+          />
+          {/* <label htmlFor="name">nome</label>
             <input
-              id='email'
-              type='email'
-              name='email'
-              value={email}
-              onChange={(e) => mudancaDeEmail(e)}
+              id='name'
+              type='text'
+              name='nome'
+              value={text}
+              onChange={(e) => mudancaDeNome(e)}
               />
-
-            <label htmlFor="password">senha</label>
+            <label htmlFor="age">idade</label>
             <input
-              id='password'
-              type='password'
-              name='senha'
-              value={password}
-              onChange={(e) => mudancaDeSenha(e)}
+              id='age'
+              type='number'
+              name='idade'
+              value={number}
+              onChange={(e) => mudancaDeIdade(e)}
               />
-              <button type='submit'>Enviar informações</button>
-          </form>
-        </div>
-      </section>
-       {/*<fieldset>
-        <legend>Nome</legend>
-        <label htmlFor="1"></label>
-        <input type="text" id='1' name='nome' />
-      </fieldset>
-      
-       <fieldset>
-        <legend>Idade</legend>
-        <label htmlFor="2"></label>
-        <input 
-        type="number"
-        id='2'
-        name='idade' />
-      </fieldset>
-      
-      <fieldset>
-        <legend>Telefone</legend>
-        <label htmlFor="3"></label>
-        <input type="number" id='3' name='telefone' />
-      </fieldset>
-      
-      <fieldset>
-        <legend>Instagram</legend>
-        <label htmlFor="4"></label>
-        <input type="text" id='4' name='instagram' />
-      </fieldset>
-      
-      <fieldset>
-        <legend>GitHub</legend>
-        <label htmlFor="5"></label>
-        <input type="text" id='5' name='github' />
-      </fieldset>
-
-      <fieldset>
-        <legend>Pensamento</legend>
-        <label htmlFor="6"></label>
-        <input type="text" id='6' name='pensamento' />
-      </fieldset>
-      
-      <fieldset>
-        <legend>Probleminha</legend>
-        <label htmlFor="7"></label>
-        <input type="text" id='7' name='probleminha' />
-      </fieldset>
-      
-      <fieldset>
-        <legend>Última série</legend>
-        <label htmlFor="8"></label>
-        <input type="text" id='8' name='serie' />
-      </fieldset>
-      
-      <fieldset>
-        <legend>Último jogo</legend>
-        <label htmlFor="9"></label>
-        <input type="text" id='9' name='jogo' />
-      </fieldset>
-      
-      <fieldset>
-        <legend>Música</legend>
-        <label htmlFor="10"></label>
-        <input type="text" id='10' name='musica' />
-      </fieldset>
-
-      <fieldset>
-        <legend>Gênero</legend>
-
-        <div>
-          <label htmlFor="masc">Masculino</label>
-          <input type="radio" id="masc" name="genero" value="masc" />
-        </div>
-
-        <div>
-          <label htmlFor="fem">Feminino</label>
-          <input type="radio" id="fem" name="genero" value="fem" />
-        </div>
-      </fieldset>
-      
-      <fieldset>
-        <legend>Habilidade especial</legend>
-        <label htmlFor="11"></label>
-        <input type="text" id='11' name='habilidade' />
-      </fieldset>
-      
-      <fieldset>
-        <legend>Poder especial</legend>
-        <label htmlFor="12"></label>
-        <input type="text" id='12' name='poder' />
-      </fieldset>
-      
-      <fieldset>
-        <legend>Time que você torce</legend>
-        <label htmlFor="13"></label>
-        <input type="text" id='13' name='time' />
-      </fieldset>
-      
-       */}
-    </>
-  )
+            <label htmlFor=""></label>
+            <input
+              id=''
+              type=''
+              name=''
+              value={}
+              onChange={(e) => mudancaDeTelefone(e)}
+              />
+            <label htmlFor=""></label>
+            <input
+              id=''
+              type=''
+              name=''
+              value={}
+              onChange={(e) => mudancaDePerfil(e)}
+              />
+            <label htmlFor=""></label>
+            <input
+              id=''
+              type=''
+              name=''
+              value={}
+              onChange={(e) => mudancaDeUsuario(e)}
+              />
+            <label htmlFor=""></label>
+            <input
+              id=''
+              type=''
+              name=''
+              value={}
+              onChange={(e) => mudancaDePensamento(e)}
+              />
+            <label htmlFor=""></label>
+            <input
+              id=''
+              type=''
+              name=''
+              value={}
+              onChange={(e) => mudancaDeProblema(e)}
+              />
+            <label htmlFor=""></label>
+            <input
+              id=''
+              type=''
+              name=''
+              value={}
+              onChange={(e) => mudancaDeSerie(e)}
+              />
+            <label htmlFor=""></label>
+            <input
+              id=''
+              type=''
+              name=''
+              value={}
+              onChange={(e) => mudancaDeJogo(e)}
+              />
+            <label htmlFor=""></label>
+            <input
+              id=''
+              type=''
+              name=''
+              value={}
+              onChange={(e) => mudancaDeMusica(e)}
+              />
+            <label htmlFor=""></label>
+            <input
+              id=''
+              type=''
+              name=''
+              value={}
+              onChange={(e) => mudancaDeGenero(e)}
+              />
+            <label htmlFor=""></label>
+            <input
+              id=''
+              type=''
+              name=''
+              value={}
+              onChange={(e) => mudancaDeHabilidade(e)}
+              />
+            <label htmlFor=""></label>
+            <input
+              id=''
+              type=''
+              name=''
+              value={}
+              onChange={(e) => mudancaDePoder(e)}
+              />
+            <label htmlFor=""></label>
+            <input
+              id=''
+              type=''
+              name=''
+              value={}
+              onChange={(e) => mudancaDeTime(e)}
+              />*/}
+          <button type='submit'>Enviar informações</button>
+        </form>
+      </div>
+    </section>
+  </>
+)
 }
 
 export default App
